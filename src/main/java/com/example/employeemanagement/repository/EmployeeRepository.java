181b86dbff4d270d7f5400f6c95776cf48cdc132
TheComputationalCore <dineshchandra962@gmail.com>
2025-11-28 00:56:38 +0530

    Update EmployeeRepository.java

--- src/main/java/com/example/employeemanagement/repository/EmployeeRepository.java
+++ src/main/java/com/example/employeemanagement/repository/EmployeeRepository.java
@@ -1,19 +1,16 @@
 package com.example.employeemanagement.repository;
 
 import com.example.employeemanagement.model.Employee;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     boolean existsByEmail(String email);
 
-    Page<Employee> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCaseOrEmailContainingIgnoreCaseOrDepartmentContainingIgnoreCase(
-            String firstName,
-            String lastName,
-            String email,
-            String department,
-            Pageable pageable
-    );
+    long countDistinctDepartment();
+
+    long countDistinctPosition();
 }
