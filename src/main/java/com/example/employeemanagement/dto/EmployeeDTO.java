d0bce6d9ecea685fb398d182e6efef77c3c43e60
TheComputationalCore <dineshchandra962@gmail.com>
2025-11-28 03:03:00 +0530

    Update EmployeeDTO.java

--- src/main/java/com/example/employeemanagement/dto/EmployeeDTO.java
+++ src/main/java/com/example/employeemanagement/dto/EmployeeDTO.java
@@ -1,15 +1,27 @@
 package com.example.employeemanagement.dto;
 
-import lombok.Data;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDTO {
+
     private Long id;
+
     private String firstName;
+
     private String lastName;
+
     private String email;
+
     private String phoneNumber;
+
     private String department;
+
     private String position;
+
     private Double salary;
 }
