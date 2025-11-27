bd05a779bd5b576f8c42bf557ae844cf03574376
TheComputationalCore <dineshchandra962@gmail.com>
2025-11-28 01:56:27 +0530

    Update EmployeeDTO.java

--- src/main/java/com/example/employeemanagement/dto/EmployeeDTO.java
+++ src/main/java/com/example/employeemanagement/dto/EmployeeDTO.java
@@ -4,25 +4,12 @@ import lombok.Data;
 
 @Data
 public class EmployeeDTO {
-
     private Long id;
-
     private String firstName;
-
     private String lastName;
-
     private String email;
-
     private String phoneNumber;
-
     private String department;
-
     private String position;
-
     private Double salary;
-
-    // Computed field (not in DB)
-    public String getFullName() {
-        return firstName + " " + lastName;
-    }
 }
