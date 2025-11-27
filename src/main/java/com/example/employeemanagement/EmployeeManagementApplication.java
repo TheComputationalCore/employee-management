55e3dcd72cb400bd893da2d51c30b10af46c50c1
TheComputationalCore <dineshchandra962@gmail.com>
2025-11-27 23:35:54 +0530

    Update EmployeeManagementApplication.java

--- src/main/java/com/example/employeemanagement/EmployeeManagementApplication.java
+++ src/main/java/com/example/employeemanagement/EmployeeManagementApplication.java
@@ -1,4 +1,4 @@
-package com.thecomputationalcore.employeemanagement;
+package com.example.employeemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
