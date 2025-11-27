15e02d98ee01c8e67e85bd490994f8219da26d13
TheComputationalCore <dineshchandra962@gmail.com>
2025-11-27 23:35:11 +0530

    Update SwaggerConfig.java

--- src/main/java/com/example/employeemanagement/config/SwaggerConfig.java
+++ src/main/java/com/example/employeemanagement/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.thecomputationalcore.employeemanagement.config;
+package com.example.employeemanagement.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
