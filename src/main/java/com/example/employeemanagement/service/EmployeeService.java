3b836d57a1578991cec509065c76b2e6fa57e7f7
TheComputationalCore <dineshchandra962@gmail.com>
2025-11-27 22:26:36 +0530

    Update EmployeeService.java

--- src/main/java/com/example/employeemanagement/service/EmployeeService.java
+++ src/main/java/com/example/employeemanagement/service/EmployeeService.java
@@ -1,20 +1,22 @@
-package com.example.employeemanagement.service;
+package com.thecomputationalcore.employeemanagement.service;
 
-import com.example.employeemanagement.model.Employee;
+import com.thecomputationalcore.employeemanagement.dto.EmployeeDto;
+
 import java.util.List;
 
 public interface EmployeeService {
-    List<Employee> getAllEmployees();
-    
-    Employee getEmployeeById(Long id);
-    
-    Employee createEmployee(Employee employee);
-    
-    Employee updateEmployee(Long id, Employee employeeDetails);
-    
+
+    List<EmployeeDto> getAllEmployees();
+
+    EmployeeDto getEmployeeById(Long id);
+
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto updateEmployee(Long id, EmployeeDto employeeDto);
+
     void deleteEmployee(Long id);
-    
-    List<Employee> getEmployeesByDepartment(String department);
-    
-    List<Employee> getEmployeesByPosition(String position);
-} 
+
+    List<EmployeeDto> getEmployeesByDepartment(String department);
+
+    List<EmployeeDto> getEmployeesByPosition(String position);
+}
