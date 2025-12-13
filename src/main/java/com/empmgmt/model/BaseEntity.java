1f8eca7e3871315a822b2789a0aeb895b2f3e339
TheComputationalCore <dineshchandra962@gmail.com>
2025-12-13 23:28:59 +0530

    Update BaseEntity.java

--- src/main/java/com/empmgmt/model/BaseEntity.java
+++ src/main/java/com/empmgmt/model/BaseEntity.java
@@ -1,8 +1,12 @@
 package com.empmgmt.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
 import lombok.Getter;
 import lombok.Setter;
+
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -18,16 +22,18 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity {
 
     @CreatedDate
-    @Column(updatable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
     @CreatedBy
-    @Column(updatable = false)
+    @Column(name = "created_by", nullable = false, updatable = false)
     private String createdBy;
 
     @LastModifiedBy
+    @Column(name = "updated_by")
     private String updatedBy;
 }
