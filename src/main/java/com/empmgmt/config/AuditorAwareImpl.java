6b42b312c4ca9f7c1dc7b89caa0aec0b1f6a013b
TheComputationalCore <dineshchandra962@gmail.com>
2025-12-13 23:36:56 +0530

    Update AuditorAwareImpl.java

--- src/main/java/com/empmgmt/config/AuditorAwareImpl.java
+++ src/main/java/com/empmgmt/config/AuditorAwareImpl.java
@@ -1,7 +1,6 @@
 package com.empmgmt.config;
 
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -14,17 +13,12 @@ public class AuditorAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
 
-        Authentication authentication = SecurityContextHolder
-                .getContext()
-                .getAuthentication();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-        if (authentication == null ||
-            !authentication.isAuthenticated() ||
-            authentication instanceof AnonymousAuthenticationToken) {
-
+        if (auth == null || !auth.isAuthenticated() || auth.getPrincipal().equals("anonymousUser")) {
             return Optional.of("SYSTEM");
         }
 
-        return Optional.ofNullable(authentication.getName());
+        return Optional.of(auth.getName());
     }
 }
